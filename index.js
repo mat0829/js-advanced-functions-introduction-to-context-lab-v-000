91924c3f8c80a73d2ab6a7f1ded91b3edd875577
Mat Anderson <matandersonlv@hotmail.com>
2020-02-13 23:58:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,6 +21,6 @@ let createTimeInEvent = function(employee, dateStamp) {
   let [date, hour] = dateStamp.split(' ')
   employee.timeInEvents.push({
     type: "timeIn",
-    hour: parseInt
+    hour: parseInt(hour, 10)
   })
 }
