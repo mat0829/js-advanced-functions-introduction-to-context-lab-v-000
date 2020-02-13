d533387ab4653645e18b4b920eb85ec26255c695
Mat Anderson <matandersonlv@hotmail.com>
2020-02-13 23:58:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ let createEmployeeRecords = function(employeeRowData) {
 let createTimeInEvent = function(employee, dateStamp) {
   let [date, hour] = dateStamp.split(' ')
   employee.timeInEvents.push({
-    type: "timeIn"
+    type: "timeIn",
+    
   })
 }
